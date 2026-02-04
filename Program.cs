1959c2a42ff2949b45eea4f32e5bae3eac7eafc2
Martin Selin <seleeto@gmail.com>
2026-02-04 17:06:40 +0100

    testcomment

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Controllers (om du har controllers)
+// Controllers (om du har controllers). och det har jag ju
 builder.Services.AddControllers();
 
 // Swagger
