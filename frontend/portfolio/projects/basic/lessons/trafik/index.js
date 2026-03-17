47efd60e389ab1024d6dc7c95770ef3387d36e2d
Martin Selin <seleeto@gmail.com>
2026-03-17 16:49:28 +0100

    zena js

--- frontend/portfolio/projects/basic/lessons/trafik/index.js
+++ frontend/portfolio/projects/basic/lessons/trafik/index.js
@@ -1,18 +1,36 @@
 function myLight(myColor) {
   let redbutton = document.getElementById("myProgress-" + myColor);
-
+  const lights = document.querySelectorAll(".light");
   switch (myColor) {
     case "red":
+      turnTraficLightOff(lights);
       redbutton.style.backgroundColor = myColor;
       break;
     case "yellow":
-      redbutton.style.backgroundColor = myColor;
+      if(document.getElementById("myProgress-red").style.backgroundColor === "red")
+      {
+        document.getElementById("myProgress-red").style.backgroundColor = "red";
+        redbutton.style.backgroundColor = myColor;
+      }else{
+       turnTraficLightOff(lights);
+        redbutton.style.backgroundColor = myColor;
+      }
       break;
     case "green":
+      turnTraficLightOff(lights);
       redbutton.style.backgroundColor = myColor;
       break;
-
+    case "grey":      
+      turnTraficLightOff(lights);
+    break;
     default:
       redbutton.style.backgroundColor = "grey";
+  }
+
+  
+  function turnTraficLightOff(lights){
+     for (let i = 0; i < lights.length; i++) {
+        lights[i].style.backgroundColor = "grey";
+      }
   }
 }
